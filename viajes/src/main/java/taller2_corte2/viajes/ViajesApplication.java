ef64531d08560a579348f3ecffbdc7d0a2b33b6c
NesANTIME <jacpketmovies@gmail.com>
2025-10-13 10:30:27 -0500

    V1
    V1

---
+++ viajes/src/main/java/taller2_corte2/viajes/ViajesApplication.java
@@ -0,0 +1,13 @@
+package taller2_corte2.viajes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViajesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViajesApplication.class, args);
+	}
+
+}
---
+++ viajes/src/test/java/taller2_corte2/viajes/ViajesApplicationTests.java
@@ -0,0 +1,13 @@
+package taller2_corte2.viajes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViajesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
